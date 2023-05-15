0558689739a2d1d21224b64bff26cdb6583eb546
Hitboxozisan <gaiya0014@gmail.com>
2023-05-16 00:44:06 +0900

    スカイドーム実装中。Objファイルは別途保存

---
+++ DirectX11_Pro/SphereMap.cpp
@@ -0,0 +1,191 @@
+#include "SphereMap.h"
+
+
+HRESULT SphereMap::Init(ID3D11Device* inDevice, ID3D11DeviceContext* inContext, IDXGISwapChain* inSwap)
+{
+	device = inDevice;
+	deviceContext = inContext;
+	swapChain = inSwap;
+
+	// シェーダー初期化
+	if (FAILED(InitShader()))
+	{
+		return E_FAIL;
+	}
+	// メッシュ作成
+	if (FAILED(InitStaticMesh("Data/Model/ShereMap/SkyDome.obj", &mesh)))
+	{
+		return E_FAIL;
+	}
+
+	return S_OK;
+}
+
+HRESULT SphereMap::InitStaticMesh(LPSTR FileName, SphereMapMesh* pMesh)
+{
+	float x, y, z;
+	int v1 = 0, v2 = 0, v3 = 0;
+	int vn1 = 0, vn2 = 0, vn3 = 0;
+	int vt1 = 0, vt2 = 0, vt3 = 0;
+	DWORD dwVCount = 0;//読み込みカウンター
+	DWORD dwVTCount = 0;//読み込みカウンター
+	DWORD dwVNCount = 0;
+	DWORD dwFCount = 0;//読み込みカウンター
+
+	char key[200] = { 0 };
+	//OBJファイルを開いて内容を読み込む
+	FILE* fp = NULL;
+	fopen_s(&fp, FileName, "rt");
+
+	//事前に頂点数、ポリゴン数を調べる
+	while (!feof(fp))
+	{
+		//キーワード読み込み
+		fscanf_s(fp, "%s ", key, sizeof(key));
+		//頂点
+		if (strcmp(key, "v") == 0)
+		{
+			pMesh->dwNumVert++;
+		}
+		// 法線
+		if (strcmp(key, "vn") == 0)
+		{
+			dwVNCount++;
+		}
+		//テクスチャー座標
+		if (strcmp(key, "vt") == 0)
+		{
+			dwVTCount++;
+		}
+		//フェイス（ポリゴン）
+		if (strcmp(key, "f") == 0)
+		{
+			pMesh->dwNumFace++;
+		}
+	}
+
+	//一時的なメモリ確保（頂点バッファとインデックスバッファ）
+	ObjVertex* pvVertexBuffer = new ObjVertex[pMesh->dwNumFace * 3];
+	XMFLOAT3* pvCoord = new XMFLOAT3[pMesh->dwNumVert];
+	XMFLOAT3* pvNormal = new XMFLOAT3[dwVNCount];
+	XMFLOAT2* pvTexture = new XMFLOAT2[dwVTCount];
+	int* piFaceBuffer = new int[pMesh->dwNumFace * 3];//３頂点ポリゴンなので、1フェイス=3頂点(3インデックス)
+
+	//本読み込み	
+	fseek(fp, SEEK_SET, 0);
+	dwVCount = 0;
+	dwVTCount = 0;
+	dwVNCount = 0;
+	dwFCount = 0;
+
+	while (!feof(fp))
+	{
+		//キーワード 読み込み
+		ZeroMemory(key, sizeof(key));
+		fscanf_s(fp, "%s ", key, sizeof(key));
+
+		// マテリアル読み込み
+		if (strcmp(key, "mtllib") == 0)
+		{
+			fscanf_s(fp, "%s ", key, sizeof(key));
+			LoadMaterial("Data/Material/Geometry+Normal+UV.mtl", &material);
+		}
+		//頂点 読み込み
+		if (strcmp(key, "v") == 0)
+		{
+			fscanf_s(fp, "%f %f %f", &x, &y, &z);
+			pvCoord[dwVCount].x = -x;//OBJは右手座標系なのでxあるいはｚを反転
+			pvCoord[dwVCount].y = y;
+			pvCoord[dwVCount].z = z;
+			dwVCount++;
+		}
+		//法線 読み込み
+		if (strcmp(key, "vn") == 0)
+		{
+			fscanf_s(fp, "%f %f %f", &x, &y, &z);
+			pvNormal[dwVNCount].x = -x;
+			pvNormal[dwVNCount].y = y;//OBJファイルはY成分が逆なので合わせる
+			pvNormal[dwVNCount].z = z;
+			dwVNCount++;
+		}
+		// テクスチャ座標読み込み
+		if (strcmp(key, "vt") == 0)
+		{
+			fscanf_s(fp, "%f %f", &x, &y);
+			pvTexture[dwVTCount].x = x;
+			pvTexture[dwVTCount].y = 1 - y;
+			dwVTCount++;
+		}
+		//フェイス 読み込み→頂点インデックスに
+		if (strcmp(key, "f") == 0)
+		{
+			fscanf_s(fp, "%d/%d/%d %d/%d/%d %d/%d/%d", &v1, &vt1, &vn1, &v2, &vt2, &vn2, &v3, &vt3, &vn3);
+			piFaceBuffer[dwFCount * 3] = dwFCount * 3;
+			piFaceBuffer[dwFCount * 3 + 1] = dwFCount * 3 + 1;
+			piFaceBuffer[dwFCount * 3 + 2] = dwFCount * 3 + 2;
+			//頂点構造体に代入
+			pvVertexBuffer[dwFCount * 3].pos = pvCoord[v1 - 1];
+			pvVertexBuffer[dwFCount * 3].norm = pvNormal[vn1 - 1];
+			pvVertexBuffer[dwFCount * 3].tex = pvTexture[vt1 - 1];
+			pvVertexBuffer[dwFCount * 3 + 1].pos = pvCoord[v2 - 1];
+			pvVertexBuffer[dwFCount * 3 + 1].norm = pvNormal[vn2 - 1];
+			pvVertexBuffer[dwFCount * 3 + 1].tex = pvTexture[vt2 - 1];
+			pvVertexBuffer[dwFCount * 3 + 2].pos = pvCoord[v3 - 1];
+			pvVertexBuffer[dwFCount * 3 + 2].norm = pvNormal[vn3 - 1];
+			pvVertexBuffer[dwFCount * 3 + 2].tex = pvTexture[vt3 - 1];
+			dwFCount++;
+		}
+	}
+
+	fclose(fp);
+
+	//バーテックスバッファーを作成
+	D3D11_BUFFER_DESC bd;
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(ObjVertex) * pMesh->dwNumFace * 3;
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+	bd.MiscFlags = 0;
+	D3D11_SUBRESOURCE_DATA InitData;
+	InitData.pSysMem = pvVertexBuffer;
+	if (FAILED(device->CreateBuffer(&bd, &InitData, &pMesh->vertexBuffer)))
+	{
+		return E_FAIL;
+	}
+
+	//インデックスバッファーを作成
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(int) * pMesh->dwNumFace * 3;
+	bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+	bd.MiscFlags = 0;
+	InitData.pSysMem = piFaceBuffer;
+	if (FAILED(device->CreateBuffer(&bd, &InitData, &pMesh->indexBuffer)))
+	{
+		return E_FAIL;
+	}
+
+	//一時的な入れ物は、もはや不要
+	delete[] pvCoord;
+	delete[] pvTexture;
+	delete[] pvNormal;
+	delete[] pvVertexBuffer;
+	delete[] piFaceBuffer;
+
+	return S_OK;
+}
+
+HRESULT SphereMap::InitShader()
+{
+	return E_NOTIMPL;
+}
+
+HRESULT SphereMap::MakeTexture()
+{
+	return E_NOTIMPL;
+}
+
+HRESULT SphereMap::LoadMaterial(LPSTR FileName, SphereMapMaterial* pMaterial)
+{
+	return E_NOTIMPL;
+}
---
+++ DirectX11_Pro/SphereMap.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "Common.h"
+#include "Framework.h"
+
+using namespace DirectX;
+
+struct SphereMapMesh
+{
+	DWORD dwNumVert = 0;
+	DWORD dwNumFace = 0;
+	ID3D11Buffer* vertexBuffer;
+	ID3D11Buffer* indexBuffer;
+};
+
+struct SphereMapMaterial
+{
+	CHAR name[111];
+	XMFLOAT4 ka;
+	XMFLOAT4 kd;
+	XMFLOAT4 ks;
+	CHAR textureName[110];
+};
+
+
+/// <summary>
+/// 環境マッピング（スフィアマップ）
+/// </summary>
+class SphereMap
+{
+public:
+	HRESULT Init(ID3D11Device* inDevice, ID3D11DeviceContext* inContext, IDXGISwapChain* inSwap);
+	HRESULT InitStaticMesh(LPSTR FileName, SphereMapMesh* pMesh);
+	HRESULT InitShader();
+	HRESULT MakeTexture();
+	HRESULT LoadMaterial(LPSTR FileName, SphereMapMaterial* pMaterial);
+
+	void Render();
+
+private:
+
+	struct ObjVertex
+	{
+		XMFLOAT3 pos = { 0.0f, 0.0f, 0.0f };		// 位置
+		XMFLOAT3 norm = { 0.0f, 0.0f, 0.0f };
+		XMFLOAT2 tex = { 0.0f, 0.0f };
+	};
+
+	struct ObjShaderConstantBuffer
+	{
+		XMMATRIX W;
+		XMMATRIX WVP;
+		XMFLOAT4 lightDir;
+		XMFLOAT4 diffuse;
+		XMFLOAT4 specular;
+		XMFLOAT4 eye;
+	};
+
+	HWND hWnd;
+	ID3D11Device* device;
+	ID3D11DeviceContext* deviceContext;
+	IDXGISwapChain* swapChain;
+
+
+	// テクスチャ
+	SphereMapMesh mesh;
+	SphereMapMaterial material;
+};
+
